1baabe4d81f105ebfea779dc458fca2da97a292b
Jennifer Ruttle <jeruttle@yorku.ca>
2019-11-22 12:37:40 -0500

    began analysis of behavioural data

---
+++ R/Semantic_Behavioural_Analysis.R
@@ -0,0 +1,9 @@
+ImageNo<- c(1:80)
+ImageName<- c('ac unit','flower', 'router', 'computer bag', 'barrel', 'battery', 'birds nest', 'belt', 'scale', 'shell', 'googles', 'shoe', 'bench', 
+              'computer speaker', 'speaker', 'kids toy', 'basket', 'teddy bear', 'satchel', 'sunglasses', 'table', 'tape', 'tent', 'twine', 'kleenex',
+              'bow tie', 'train', 'tupperwear', 'toque', 'vhs', 'fit box', 'vhs', 'volleyball', 'washing machine', 'watch', 'watermelon', 'box', ' bucket',
+              'button', 'candelabra', 'candle', 'car', 'mirror', 'champagne', 'chest', 'clock', 'coffee beans', 'change loonies', 'cooler', 'couch', 'desk',
+              'cd holder', 'cables',' faucet', 'feather', 'flipflop', 'floopy disk','foot stool', 'presents', 'hard hat', 'hat','shoe', 'shoe', 'jar', 'jeans',
+              'tractor', 'lemon', 'basketball net', 'leather desk chair', 'clock', 'outlet', 'soother', 'peanuts', 'bottle', 'pinecone', 'flower pot',
+              'mailbox', 'printer', 'cone', 'boombox')
+Answers<-data.frame(ImageNo, ImageName)
