a36de2c681083d4f626f47ef81f6de8550c775e9
Jenn Ruttle <jennruttle@gmail.com>
2020-01-12 17:30:05 -0500

    analysis on beta values from younger adults activation maps

---
+++ R/Analysis.R
@@ -0,0 +1,15 @@
+ANOVAanalysis<- function(AllDataANOVA){
+  AllDataANOVA$ID<- as.factor(AllDataANOVA$ID)
+  AllDataANOVA$Object_Type<- as.factor(AllDataANOVA$Object_Type)
+  AllDataANOVA$Component_Value<- as.factor(AllDataANOVA$Component_Value)
+  AllDataANOVA$Measurement_Type<- as.factor(AllDataANOVA$Measurement_Type)
+  AllDataANOVA$ROI_Location<- as.factor(AllDataANOVA$ROI_Location)
+  fullmodel <- ezANOVA(data=AllDataANOVA,
+                       dv=Betas,
+                       wid=ID,
+                       within=.(Component_Value,ROI_Location),
+                       between = Object_Type,
+                       type=3,
+                       return_aov=TRUE)
+  return(fullmodel)
+}
