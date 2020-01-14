e1c245b88ab9dcff20bd3a0bfc4b480710783d62
Jenn Ruttle <jennruttle@gmail.com>
2020-01-13 23:03:40 -0500

    Poster Plots

---
+++ R/LOVE_poster_figs.R
@@ -0,0 +1,4 @@
+##BarPlots for LOVE poster
+##I want to make plots of the average sensitivity for each ROI and each component
+##I want to make plots of location of maximum sensitivity for each ROI and component
+##I want to make a plot of the average spatial correlation for each ROI - dont have it for both components
