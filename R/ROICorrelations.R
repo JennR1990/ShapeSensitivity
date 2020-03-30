9c6005d4e37d773653411ab6363e499b5d140264
Jenn Ruttle <jennruttle@gmail.com>
2020-03-30 18:27:03 -0400

    MOved files around to work better with function call

--- R/ROICorrelations.R
+++ R/ROICorrelations.R
@@ -1,11 +1,17 @@
-Novel_behavioural_accuracy_per_subject <- read_csv("Novel_behavioural_accuracy_per_subject.csv")
-Sem <- read_csv("Semantic_Performance.csv")
 
+ROIpartialcorrelation<- function(){
 
-
-
-Novel_behavioural_accuracy_per_subject$Object_Type <- "Novel"
+Novel_behavioural_accuracy_per_subject <- read_csv("../Novel_behavioural_accuracy_per_subject.csv")
+Sem <- read_csv("../Semantic_Performance.csv")
 beh<- rbind(Sem, Novel_behavioural_accuracy_per_subject)
+ROIS<- c("lh_v1v_center", "lh_v2v_center", "lh_v3v_center", "lh_hv4", "lh_vo1", "lh_vo2", "lh_ph1", "lh_ph2", 
+         "afs_l", "lh_lo1", "lh_lo2", "lh_to1", "lh_v1d_centre", "lh_v2d_centre", "lh_v3d_centre", "lh_v3a","lh_v3b",
+         "lh_ips0", "lh_ips1", "lh_ips2", "lh_ips3", "lh_ips4", "lh_aIPS", "rh_v1v_center", "rh_v2v_center", 
+         "rh_v3v_center", "rh_hv4", "rh_vo1", "rh_vo2", "rh_ph1", "rh_ph2", "afs_r", "rh_lo1", "rh_lo2", "rh_to1",
+         "rh_v1d_centre", "rh_v2d_centre", "rh_v3d_centre", "rh_v3a","rh_v3b","rh_ips0", "rh_ips1", "rh_ips2",
+         "rh_ips3", "rh_ips4", "rh_aIPS","Participant", "Object_Type")
+
+
 
 NovelPartialCorrs<- data.frame()
 for (sub in 1:12){
@@ -34,6 +40,9 @@ for (sub in 1:12){
 }
 NovelPartialCorrs<- data.frame(NovelPartialCorrs)
 NovelPartialCorrs$Object_Type<- 'Novel'
+names(NovelPartialCorrs)<- ROIS
+nmeans<-colMeans(NovelPartialCorrs[,1:46])
+barplot(nmeans, las = 2, cex.names = .75, main = "Novel Partial Correlations", ylab = "Partial Correlation")
 
 
 
@@ -63,23 +72,9 @@ for (sub in 1:12){
   
 }
 SemanticPartialCorrs<- data.frame(SemanticPartialCorrs)
-
 SemanticPartialCorrs$Object_Type<- 'Semantic'
-
-ROIS<- c("lh_v1v_center", "lh_v2v_center", "lh_v3v_center", "lh_hv4", "lh_vo1", "lh_vo2", "lh_ph1", "lh_ph2", 
-         "afs_l", "lh_lo1", "lh_lo2", "lh_to1", "lh_v1d_centre", "lh_v2d_centre", "lh_v3d_centre", "lh_v3a","lh_v3b",
-         "lh_ips0", "lh_ips1", "lh_ips2", "lh_ips3", "lh_ips4", "lh_aIPS", "rh_v1v_center", "rh_v2v_center", 
-         "rh_v3v_center", "rh_hv4", "rh_vo1", "rh_vo2", "rh_ph1", "rh_ph2", "afs_r", "rh_lo1", "rh_lo2", "rh_to1",
-         "rh_v1d_centre", "rh_v2d_centre", "rh_v3d_centre", "rh_v3a","rh_v3b","rh_ips0", "rh_ips1", "rh_ips2",
-         "rh_ips3", "rh_ips4", "rh_aIPS","Participant", "Object_Type")
-
-
 names(SemanticPartialCorrs)<- ROIS
-names(NovelPartialCorrs)<- ROIS
-#Correlations<- rbind(SemanticPartialCorrs, NovelPartialCorrs)
-
-nmeans<-colMeans(NovelPartialCorrs[,1:46])
-barplot(nmeans, las = 2, cex.names = .75, main = "Novel Partial Correlations", ylab = "Partial Correlation")
 smeans<-colMeans(SemanticPartialCorrs[,1:46])
 barplot(smeans, las = 2, cex.names = .75, main = "Semantic Partial Correlations", ylab = "Partial Correlation")
 
+}
