a09c34d52b4f75177443b7a89a6ae0be0013937e
Jenn Ruttle <jennruttle@gmail.com>
2020-04-17 13:21:21 -0400

    Added pdf figures and figures for different types of regressions

--- R/Beta_Plots.R
+++ R/Beta_Plots.R
@@ -18,8 +18,8 @@ ROIS<- c("lh_v1v_center", "lh_v2v_center", "lh_v3v_center", "lh_hv4", "lh_vo1", 
     filename<-sprintf("novel_beta_sub%.0f.csv", sub)
     data1<- read.csv(filename, header = FALSE)
     newfile<- sprintf('Sub %.0f Beta Values.svg', sub)
-    svglite(file=newfile, width=15, height=20, system_fonts=list(sans = "Arial"))
-    layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45), nrow=9, byrow=TRUE), heights=c(1,1,1,1,1))   
+    svglite(file=newfile, width=18, height=20, system_fonts=list(sans = "Arial"))
+    layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45), nrow=5, byrow=TRUE), heights=c(1,1,1,1,1))   
     for (ROI in 2:47){
       print(ROI)
       if (ROI == 10 | ROI == 33) {
@@ -30,11 +30,11 @@ ROIS<- c("lh_v1v_center", "lh_v2v_center", "lh_v3v_center", "lh_hv4", "lh_vo1", 
       betas2<- as.numeric(unlist(data1[1:5,ROI]))
       #perf<-as.numeric(unlist(beh[beh$Participant == sub & beh$Object_Type == "Semantic",1:5]))
       control<- c(1:5)
-      plot(x = control, y = betas1, main = ROIS[ROI-1], type = 'l', col = 'Blue', ylim = c(-3.5,3), axes = FALSE, ylab = "Beta Values", xlab = "Scrambling Level")
+      plot(x = control, y = betas1, main = ROIS[ROI-1], type = 'l', col = 'Blue', ylim = c(-2,3), axes = FALSE, ylab = "Beta Values", xlab = "Scrambling Level")
       axis(1, at = c(1:5), labels = c('Intact', 'S4', "S16", 'S64', 'S256'))
-      axis(2, at = c(-3,-2,-1,0,1,2,3), las = 2)
+      axis(2, at = c(-2,-1,0,1,2,2.5), las = 2)
       lines(x = control, y = betas2, col = 'Green')
-      legend(2,-1.5, legend = c("Semantic", "Novel"), col = c('Blue', 'Green'), lty = 1, bty = 'n')
+      legend(2,-1, legend = c("Semantic", "Novel"), col = c('Blue', 'Green'), lty = 1, bty = 'n')
       }
     }
     plot(x = 2, y = 0, col = 'white',axes = FALSE, ylim = c(-3.5,3), ylab = "", xlab = "")
